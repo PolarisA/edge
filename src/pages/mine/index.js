e1c464341d919c6d207ac7718c0fe84335b940c1
HankinsPan <hankins.home@outlook.com>
2019-04-13 00:10:19 +0800

    modify mine Page

--- src/pages/mine/index.js
+++ src/pages/mine/index.js
@@ -4,10 +4,23 @@
  * @Description :
  **/
 
-import Taro, { Component } from '@tarojs/taro'
-import { View, Input, Text } from '@tarojs/components'
+import Taro, {Component} from '@tarojs/taro'
+import {
+  View,
+  Input,
+  Text,
+  Button,
+  ScrollView,
+} from '@tarojs/components'
 
 import './index.scss'
+
+const profile = [
+  {title: '勋章', numbers: 5, icon: ''},
+  {title: '手机号', numbers: 0, icon: ''},
+  {title: '卡片信息', numbers: 9, icon: ''},
+  {title: '优惠券', numbers: 2, icon: ''},
+]
 
 class Mine extends Component {
   config = {
@@ -16,17 +29,48 @@ class Mine extends Component {
 
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      profile,
+    }
+  }
+
+  onItemClick = (profile) => {
+    console.log("== onItemClick  profile -=->", profile)
+
   }
 
   render() {
-    return(
-      <View className='mine page'>
-        <Text>
-          {'profile '}
-        </Text>
+    const {profile} = this.state
+    return (
+      <ScrollView className='mine-page'>
+        <View className='mine-header'>
+          <View className='mine-item-header'>
+            <Text className='header-font'>
+              登录
+            </Text>
+          </View>
 
-      </View>
+          <View className='mine-icon-size'>
+            <Text>
+              hello
+            </Text>
+          </View>
+        </View>
+
+        <View className='mine-item'>
+          {
+            profile.map((item, index) => {
+              return (
+                <View key={index} className='mine-item-view'>
+                  <Text>
+                    index
+                  </Text>
+                </View>
+              )
+            })
+          }
+        </View>
+      </ScrollView>
     )
   }
 }
