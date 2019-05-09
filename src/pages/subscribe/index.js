7dccb0bceeb4ae906f56230e3a344f2abac19a82
HankinsPan <hankins.home@outlook.com>
2019-05-09 20:43:48 +0800

    add map files

--- src/pages/subscribe/index.js
+++ src/pages/subscribe/index.js
@@ -3,14 +3,14 @@
  * @Date : 2019-04-08
  * @Description :
  **/
-import Taro, { Component } from '@tarojs/taro'
-import { View, Input, Text, Map } from '@tarojs/components'
+import Taro, {Component} from '@tarojs/taro'
+import {View, Input, Text, Map} from '@tarojs/components'
 
 import './index.scss'
-import { connect } from '@tarojs/redux'
-import { dispatcher } from '@opcjs/zoro'
+import {connect} from '@tarojs/redux'
+import {dispatcher} from '@opcjs/zoro'
 
-@connect(({ discover }) => ({ discover }))
+@connect(({discover}) => ({discover}))
 class Subscribe extends Component {
   config = {
     navigationBarTitleText: '订阅'
@@ -18,11 +18,48 @@ class Subscribe extends Component {
 
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      center: {
+        latitude: 0,
+        longitude: 0,
+      },
+      loading: false
+    }
+  }
+
+  componentDidMount() {
+    Taro.getLocation({
+      type: 'gcj02',
+      altitude: true,//高精度定位
+      success: function (res) {
+        console.log("=== location success ===", res)
+      },
+      fail: function () {
+        console.log("=== location fail ===")
+      }
+    }).then(res => {
+      this.setLocation(res)
+    })
+  }
+
+  async setLocation(res) {
+    const {latitude, longitude} = res
+    console.log("=== setLocation res -=-> ", res)
+    const center = {
+      latitude,
+      longitude,
+    }
+
+    this.setState({
+      center,
+    })
   }
 
   render() {
     console.log("=== discover props >> ", this.props)
+    console.log("=== discover state >> ", this.sa)
+
+
     return (
       <View className='discover-page'>
         <Map
@@ -30,7 +67,6 @@ class Subscribe extends Component {
           longitude={116.480913}
           latitude={39.996717}
           scale={15}
-
         />
 
       </View>
