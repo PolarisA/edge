f73361f510e0c9e4193a91ba5f03cf44241a3c00
HankinsPan <hankins.home@outlook.com>
2019-05-09 11:20:17 +0800

    modify location & marker

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -45,25 +45,32 @@ const shopStatus = {
   ENOUGH: '爆满',
 }
 
+const statusColor = {
+  OPENING: '#02A348',
+  RESTING: '#D3D4D9',
+  RENOVATE: '#424C55',
+  ENOUGH: '#F6511D',
+}
+
 
 const placeInfo = [
-  { id: 1000, name: '成吉思汗大街店', area: 60, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '18', appoint: '5', activity: '首次扫码,免费3次体验', status: shopStatus.ENOUGH },
-  { id: 1001, name: '财经大学(回民区)店', area: 45, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '7', appoint: '2', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING },
-  { id: 1002, name: '工业大学(新城区)店', area: 52, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '23', appoint: '4', activity: '首次扫码,免费3次体验', status: shopStatus.RESTING },
-  { id: 1003, name: '内蒙古大学（赛罕区）店', area: 78, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '25', appoint: '7', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING },
-  { id: 1004, name: '将军衙署店', area: 43, deploy: ['跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '14', appoint: '0', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING },
-  { id: 1005, name: '鼓楼店', area: 45, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '16', appoint: '1', activity: '首次扫码,免费3次体验', status: shopStatus.ENOUGH },
-  { id: 1006, name: '青城公园店', area: 36, deploy: ['跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '24', appoint: '1', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING },
-  { id: 1007, name: '伊利广场店', area: 65, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '30', appoint: '0', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING },
-  { id: 1008, name: '十四中店', area: 72, deploy: ['跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '16', appoint: '2', activity: '首次扫码,免费3次体验', status: shopStatus.RENOVATE },
-  { id: 1009, name: '万达广场店', area: 50, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '22', appoint: '5', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING },
-  { id: 1010, name: '海亮广场店', area: 45, deploy: ['淋浴', '跑步机', '椭圆机', '陀螺仪'], NumberNow: '19', appoint: '3', activity: '首次扫码,免费3次体验', status: shopStatus.RESTING },
-  { id: 1011, name: '长乐宫店', area: 48, deploy: ['跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '27', appoint: '5', activity: '首次扫码,免费3次体验', status: shopStatus.ENOUGH },
-  { id: 1012, name: '学府康都店', area: 75, deploy: ['淋浴', '跑步机', '扩胸机', '陀螺仪'], NumberNow: '30', appoint: '8', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING },
-  { id: 1013, name: '嘉茂购物中心店', area: 60, deploy: ['跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '13', appoint: '2', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING },
-  { id: 1014, name: '民族学院店', area: 80, deploy: ['淋浴', '跑步机', '陀螺仪'], NumberNow: '40', appoint: '5', activity: '首次扫码,免费3次体验', status: shopStatus.RESTING },
-  { id: 1015, name: '国贸店', area: 55, deploy: ['跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '18', appoint: '2', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING },
-  { id: 1016, name: '体育场店', area: 90, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '45', appoint: '15', activity: '首次扫码,免费3次体验', status: shopStatus.RENOVATE },
+  { id: 1000, name: '成吉思汗大街店', area: 60, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '18', appoint: '5', activity: '首次扫码,免费3次体验', status: shopStatus.ENOUGH, typeColor: statusColor.ENOUGH },
+  { id: 1001, name: '财经大学(回民区)店', area: 45, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '7', appoint: '2', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING, typeColor: statusColor.OPENING },
+  { id: 1002, name: '工业大学(新城区)店', area: 52, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '23', appoint: '4', activity: '首次扫码,免费3次体验', status: shopStatus.RESTING, typeColor: statusColor.RESTING },
+  { id: 1003, name: '内蒙古大学（赛罕区）店', area: 78, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '25', appoint: '7', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING, typeColor: statusColor.OPENING },
+  { id: 1004, name: '将军衙署店', area: 43, deploy: ['跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '14', appoint: '0', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING, typeColor: statusColor.OPENING },
+  { id: 1005, name: '鼓楼店', area: 45, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '16', appoint: '1', activity: '首次扫码,免费3次体验', status: shopStatus.ENOUGH, typeColor: statusColor.ENOUGH },
+  { id: 1006, name: '青城公园店', area: 36, deploy: ['跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '24', appoint: '1', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING, typeColor: statusColor.OPENING },
+  { id: 1007, name: '伊利广场店', area: 65, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '30', appoint: '0', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING, typeColor: statusColor.OPENING },
+  { id: 1008, name: '十四中店', area: 72, deploy: ['跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '16', appoint: '2', activity: '首次扫码,免费3次体验', status: shopStatus.RENOVATE, typeColor: statusColor.RENOVATE },
+  { id: 1009, name: '万达广场店', area: 50, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '22', appoint: '5', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING, typeColor: statusColor.OPENING },
+  { id: 1010, name: '海亮广场店', area: 45, deploy: ['淋浴', '跑步机', '椭圆机', '陀螺仪'], NumberNow: '19', appoint: '3', activity: '首次扫码,免费3次体验', status: shopStatus.RESTING, typeColor: statusColor.RESTING },
+  { id: 1011, name: '长乐宫店', area: 48, deploy: ['跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '27', appoint: '5', activity: '首次扫码,免费3次体验', status: shopStatus.ENOUGH, typeColor: statusColor.ENOUGH },
+  { id: 1012, name: '学府康都店', area: 75, deploy: ['淋浴', '跑步机', '扩胸机', '陀螺仪'], NumberNow: '30', appoint: '8', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING, typeColor: statusColor.OPENING },
+  { id: 1013, name: '嘉茂购物中心店', area: 60, deploy: ['跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '13', appoint: '2', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING, typeColor: statusColor.OPENING },
+  { id: 1014, name: '民族学院店', area: 80, deploy: ['淋浴', '跑步机', '陀螺仪'], NumberNow: '40', appoint: '5', activity: '首次扫码,免费3次体验', status: shopStatus.RESTING, typeColor: statusColor.RESTING },
+  { id: 1015, name: '国贸店', area: 55, deploy: ['跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '18', appoint: '2', activity: '首次扫码,免费3次体验', status: shopStatus.OPENING, typeColor: statusColor.OPENING },
+  { id: 1016, name: '体育场店', area: 90, deploy: ['淋浴', '跑步机', '椭圆机', '扩胸机', '陀螺仪'], NumberNow: '45', appoint: '15', activity: '首次扫码,免费3次体验', status: shopStatus.RENOVATE, typeColor: statusColor.RENOVATE },
 ]
 
 @connect(({ home }) => ({ home }))
@@ -128,9 +135,12 @@ class Index extends Component {
     console.log("=== onScroll  ")
   }
 
-  render() {
-    console.log(">>> props -=-> ", this.props)
+  renderColor(item) {
+    let statusColor = `background-color:${item.typeColor}`
+    return statusColor
+  }
 
+  render() {
     const scrollStyle = {
       height: '150px'
     }
@@ -171,7 +181,7 @@ class Index extends Component {
           {
             placeInfo.map((item, index) => {
               return (
-                <View key={index} className='home-place-item'>
+                <View key={`${index}`} className='home-place-item'>
                   <View className='home-place-item-top'>
                     <View className='home-place-item-top-left'>
                       <View className='home-place-item-top-title'>
@@ -211,11 +221,11 @@ class Index extends Component {
                     <View className='home-place-item-bottom-content'>
                       <Image src={ic_device} className='home-item-icon'/>
                       {
-                        item.deploy.map((item, index) => {
+                        item.deploy.map((cItem, index) => {
                           return (
-                            <View key={index} className='home-place-item-device'>
+                            <View key={`${index}`} className='home-place-item-device'>
                               <Text className='place-item-content-txt'>
-                                {item}
+                                {cItem}
                               </Text>
                             </View>
                           )
@@ -231,8 +241,7 @@ class Index extends Component {
                         </Text>
                       </View>
 
-
-                      <View className='home-place-item-device'>
+                      <View className='home-place-item-status' style={this.renderColor(item)}>
                         <Text className='place-item-content-txt'>
                           {item.status}
                         </Text>
--- src/pages/subscribe/index.js
+++ src/pages/subscribe/index.js
@@ -4,11 +4,30 @@
  * @Description :
  **/
 import Taro, { Component } from '@tarojs/taro'
-import { View, Input, Text, Map } from '@tarojs/components'
+import {
+  View,
+  Input,
+  Text,
+  Map,
+  Button
+} from '@tarojs/components'
 
 import './index.scss'
 import { connect } from '@tarojs/redux'
 import { dispatcher } from '@opcjs/zoro'
+
+import ic_mark from '../../images/map/ic_mark.png'
+
+const markers = [
+  { id: 1001, iconPath: ic_mark, name: '财经大学(回民区)店', latitude: 40.8536, longitude: 111.6343, width: 50, height: 50 },
+  { id: 1002, iconPath: ic_mark, name: '成吉思汗大街店', latitude: 40.8538, longitude: 116.6506, width: 50, height: 50 },
+  { id: 1003, iconPath: ic_mark, name: '工业大学(新城区)店', latitude: 40.8462, longitude: 111.6800, width: 50, height: 50 },
+  { id: 1004, iconPath: ic_mark, name: '青城公园店', latitude: 40.8081, longitude: 111.6644, width: 50, height: 50 },
+  { id: 1005, iconPath: ic_mark, name: '伊利广场店', latitude: 40.8093, longitude: 111.6541, width: 50, height: 50 },
+  { id: 1006, iconPath: ic_mark, name: '万达广场店', latitude: 40.8329, longitude: 111.7345, width: 50, height: 50 },
+  { id: 1007, iconPath: ic_mark, name: '海亮广场店', latitude: 40.8147, longitude: 111.6697, width: 50, height: 50 },
+  { id: 1008, iconPath: ic_mark, name: '内蒙古大学（赛罕区）店', latitude: 40.8126, longitude: 111.6902, width: 50, height: 50 },
+]
 
 @connect(({ discover }) => ({ discover }))
 class Subscribe extends Component {
@@ -18,19 +37,90 @@ class Subscribe extends Component {
 
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      center: {
+        latitude: 0,
+        longitude: 0,
+      },
+      loading: false
+    }
   }
+
+  componentDidMount() {
+    Taro.getLocation({
+      type: 'wgs84',
+      altitude: true,
+      success: (res) => {
+        console.log("=== success res === ", res)
+        this.setLocationCenter(res)
+      },
+      fail: (res) => {
+        console.log("=== fail res === ", res)
+      }
+    }).then(res => {
+      console.log("=== get location res === ", res)
+    })
+  }
+
+  setLocationCenter = (res) => {
+    const { latitude, longitude } = res
+    const center = {
+      latitude,
+      longitude,
+    }
+    this.setState({
+      center,
+    })
+  }
+
+  onSearch = () => {
+    console.log("=== onSearch ")
+
+  }
+
+  getInput = (changed) => {
+    // console.log("=== getInput changed -=-> ", changed)
+    const {
+      detail: { value }
+    } = changed
+
+    console.log("=== value -=--> ", value)
+  }
+
 
   render() {
     console.log("=== discover props >> ", this.props)
+    console.log("=== discover state >> ", this.state)
+
     return (
       <View className='discover-page'>
+        <View className='discover-search-block'>
+          <Input
+            type='text'
+            placeholder='位置/健身房/站点'
+            maxLength={30}
+            className='discover-search-input'
+            placeholderClass='discover-search-placeholder-input'
+            onInput={(value) => this.getInput(value)}
+          />
+
+          <Button
+            className='discover-search-btn'
+            size='default'
+            type='primary'
+            style='background-color:#37c'
+            onClick={this.onSearch}>
+            <Text className='discover-search-btn-txt'>
+              {'搜索'}
+            </Text>
+          </Button>
+        </View>
+
         <Map
           className='map-page'
           longitude={116.480913}
           latitude={39.996717}
           scale={15}
-
         />
 
       </View>
